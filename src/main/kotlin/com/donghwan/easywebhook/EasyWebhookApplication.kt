643e6e41009450a7a1abd25f617bae4502a25cdf
dev-donghwan <ycdoit@naver.com>
2025-06-30 20:24:57 +0900

    initial commit

---
+++ src/main/kotlin/com/donghwan/easywebhook/EasyWebhookApplication.kt
@@ -0,0 +1,11 @@
+package com.donghwan.easywebhook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EasyWebhookApplication
+
+fun main(args: Array<String>) {
+    runApplication<EasyWebhookApplication>(*args)
+}
